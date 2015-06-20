c80c98e6811e46d0918f5092107f8291c5658010
telebuddy <phil@telebuddy.com.au>
2015-06-20 13:36:57 +1000

    Removed Bluetooth and Services from setting (WIP)
    Prior to latest upstream merge

--- src/resources/lib/modules/about.py
+++ src/resources/lib/modules/about.py
@@ -82,13 +82,13 @@ class about:
         except Exception, e:
             self.oe.dbg_log('about::exit', 'ERROR: (' + repr(e) + ')')
 
-    def do_wizard(self):
-        try:
-            self.oe.dbg_log('about::do_wizard', 'enter_function', 0)
-            self.oe.winOeMain.set_wizard_title(self.oe._(32317))
-            self.oe.winOeMain.set_wizard_text(self.oe._(32318))
-            self.oe.dbg_log('about::do_wizard', 'exit_function', 0)
-        except Exception, e:
-            self.oe.dbg_log('about::do_wizard', 'ERROR: (' + repr(e) + ')')
+#    def do_wizard(self):
+#        try:
+#            self.oe.dbg_log('about::do_wizard', 'enter_function', 0)
+#            self.oe.winOeMain.set_wizard_title(self.oe._(32317))
+#            self.oe.winOeMain.set_wizard_text(self.oe._(32318))
+#            self.oe.dbg_log('about::do_wizard', 'exit_function', 0)
+#        except Exception, e:
+#            self.oe.dbg_log('about::do_wizard', 'ERROR: (' + repr(e) + ')')
 
 
